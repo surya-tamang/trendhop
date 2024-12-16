50d9db8f2b2439bc44e616c28f4c25a48b149d73
Surya Tamang <155127875+surya-tamang@users.noreply.github.com>
2024-12-16 12:27:02 +0545

    Delete src directory

--- src/components/Heading.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default function Heading({ heading }) {
-    return (
-        <>
-            <h1 className='text-accent font-bold text-2xl ml-10 py-2 heading relative w-11/12'>{heading}</h1>
-        </>
-    )
-}
